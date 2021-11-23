ecb7d08143e572b41edbce001fc7cc23daf3b899
LLT <wolilitian@gmail.com>
2021-11-23 20:22:58 +0100

    Initial commit

---
+++ src/main/java/DASinghal.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DASinghal {
+
+    private Map<Integer, Integer> n_array = new HashMap<>();
+    private Map<Integer, Character> S_array = new HashMap<>();
+}
