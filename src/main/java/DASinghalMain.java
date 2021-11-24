2b2696857ef00803e5c7b77ff5ef15447b8a64ac
Xin Wan <x.wan@student.utwente.nl>
2021-11-24 23:28:54 +0100

    add test cases

--- src/main/java/DASinghalMain.java
+++ src/main/java/DASinghalMain.java
@@ -11,9 +11,11 @@ public class DASinghalMain {
     public static int PORT = 1099;
     public static List<DASinghal> stub_list = new ArrayList<>();
     public static Registry registry;
+    public static int numberOfProcesses = 3;
 
-    public static void main(String[] args) throws RemoteException {
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException, NotBoundException {
         LocateRegistry.createRegistry(PORT);
+        creatingProcesses(numberOfProcesses);
     }
 
     public static void creatingProcesses(int numberOfProcesses) throws RemoteException, AlreadyBoundException, NotBoundException {
@@ -21,14 +23,15 @@ public class DASinghalMain {
         for (int i=0; i<numberOfProcesses; i++){
             DASinghal stub = new DASinghal(i, numberOfProcesses);
             registry.bind("Singhal_"+i, stub);
+            System.out.println(stub.getS_array());
         }
 
         registry = LocateRegistry.getRegistry("localhost");
 
         for (int i=0; i<numberOfProcesses; i++){
             DASinghalRMI process_temp = (DASinghalRMI)registry.lookup("Singhal_"+i);
-        }
 
+        }
 
     }
 }
